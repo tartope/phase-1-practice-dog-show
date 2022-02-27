fb47cc9ed32ce5e2b5a8d2fdb81302e46929fcd1
Tunisia Artope <artope@yahoo.com>
2022-02-27 12:39:18 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
-document.addEventListener('DOMContentLoaded', () => {
+fetch('http://localhost:3000/dogs')
+    .then(response => response.json())
+    .then(dogsData =>{
+        // console.log(dogsData)
+        dogsData.forEach(dog =>{
+            buildRegisteredDogs(dog)
+        })
+    })
 
-})
+
+const tableBody = document.getElementById('table-body')
+console.log(tableBody)
+function buildRegisteredDogs(dog) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dogs;
+// let newDog = document.getElementById('new-dog')
+// let newSex = document.getElementById('new-sex')
+// let newBreed = document.getElementById('new-breed')
+// let wholeTable = document.getElementById('table-body')
+// document.addEventListener('DOMContentLoaded', () => {
+//     fetch('http://localhost:3000/dogs')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         dogs = data
+//     })
+//     let dogForm = document.getElementById('dog-form');
+//     dogForm.addEventListener('submit', (event) => {
+//         event.target.value;
+//         event.preventDefault();
+//     })
+    
+//     function tableBuilder (dogs){
+//         dogs.forEach(dogs =>{
+//             addDogToTable(dogs);
+//         })
+//         }
+//         function addDogToTable(){
+//             let row = document.createElement('tr')
+//             let cell = document.createElement('td')
+//         document.createElement('td').appendChild(wholeTable)
+//         newDog.textContent = newDog.value
+//         newSex.textContent = newSex.value
+//         newBreed.textContent = newBreed.value
+//     }
+
+// })
